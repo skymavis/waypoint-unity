feb8daf69b5d1a94615a5393958a410749aad70d
thienNH0 <thien.hoang.nguyen@skymavis.com>
2024-11-07 13:42:49 +0700

    test: encode function data test script

--- Samples~/Waypoint Example/WaypointExample.cs
+++ Samples~/Waypoint Example/WaypointExample.cs
@@ -160,4 +160,28 @@ public class WaypointExample : MonoBehaviour
         var data = ABI.EncodeFunctionData(readableAbi, values);
         Execute("Atia Blessing", () => Waypoint.OnCallContract(atiaShrineContractAddress, data));
     }
+
+    [UnityEditor.MenuItem("MyMenu/Encode Swap abis")]
+    static void EncodeSwapAbi()
+    {
+        string ronAddress = "0xa959726154953bae111746e265e6d754f48570e6";
+        string axsAddress = "0x3c4e17b9056272ce1b49f6900d8cfd6171a1869d";
+        string walletAddress = "0x6B190089ed7F75Fe17B3b0A17F6ebd69f72c3F63";
+        string readableAbi = "function swapExactTokensForTokens(uint256 _amountIn, uint256 _amountOutMin, address[] _path, address _to, uint256 _deadline)";
+        var swapParams = new
+        {
+            // 0.1 Ron
+            _amountIn = "1166912850812407680945",
+            _amountOutMin = "1580474142",
+            _path = new[] { "0xe514d9DEB7966c8BE0ca922de8a064264eA6bcd4", "0x0B7007c13325C48911F73A2daD5FA5dCBf808aDc" },
+            _to = "0x1d3286A3348Fa99852d147C57A79045B41c4f713",
+            _deadline = "1730877010"
+        };
+
+        var data = ABI.EncodeFunctionData(readableAbi, swapParams);
+        var correctData = "0x38ed173900000000000000000000000000000000000000000000003f422b3f3e9ff48fb1000000000000000000000000000000000000000000000000000000005e341f1e00000000000000000000000000000000000000000000000000000000000000a00000000000000000000000001d3286a3348fa99852d147c57a79045b41c4f71300000000000000000000000000000000000000000000000000000000672b16520000000000000000000000000000000000000000000000000000000000000002000000000000000000000000e514d9deb7966c8be0ca922de8a064264ea6bcd40000000000000000000000000b7007c13325c48911f73a2dad5fa5dcbf808adc";
+        Debug.Log("Encoded Function Data " + data);
+        Debug.Log("Correct encode data  " + correctData);
+        Debug.Log("Is correct " + (data == correctData));
+    }
 }
